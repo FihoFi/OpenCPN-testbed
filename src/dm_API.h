1add2187fb3e22e622e6332980224ead2507668a
Marjukka Kokkonen <Marjukka.Kokkonen@sitowise.com>
2018-09-26 09:42:28 +0300

    Adding first scetch of API between the GDAL handling functions, and the other plugin code

---
+++ src/dm_API.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifndef _DM_API_
+#define _DM_API_
+
+class /*DECL_EXP*/ dm_API
+{
+    //dm_API();
+
+    /** Destructor making this class abstract. */
+    virtual ~dm_API() = 0;
+
+
+    virtual bool openDataSet(const char* filename);
+    //MRJ: is the same as bool setFileName(const char* filename);
+    
+     /**
+    * Returns the whole raster data in the Dataset.
+    * The coordinate span of the dataset is returned in the parameters.
+    *
+    * @param[out] topLeftOut
+    * @param[out] botRightOut
+    */
+    virtual unsigned char * getRasterData(
+        coord &topLeftOut, coord &botRightOut) const;
+
+    /**
+    * Returns a square part of the Dataset spanned by the two given (In)
+    * coordinate pairs.
+    * The coordinate span of the returned Dataset part  is returned
+    * in the (Out) parameters.
+    *
+    * @param[in] topLeftIn  
+    * @param[in] botRightIn
+    * @param[out] topLeftOut
+    * @param[out] botRightOut
+    */
+    virtual unsigned char * getRasterData(
+        const coord topLeftIn, const coord botRightIn,
+        coord &topLeftOut, coord &botRightOut) const;
+};
+
+struct coord {
+    /*float*/double lon; // mathematical, not geodetic x
+    /*float*/double lat; // mathematical, not geodetic y
+};
+
+#endif _DM_API_
