501924bfa85e57780137cc2e504f67199797d60c
Marjukka Kokkonen <Marjukka.Kokkonen@sitowise.com>
2018-10-03 12:26:52 +0300

    +Add dm_configAPI helper class +Add load, and save functions for color options

--- src/dmConfigHandler.cpp
+++ src/dmConfigHandler.cpp
@@ -7,6 +7,7 @@ bool dmConfigHandler::LoadConfig(void)
 {
     bool success = true;
     m_pconfig->SetPath(_T("/Settings/LIVI_Depth_model_pi"));
+
     wxString pluginConfPathStr;
     success &= m_pconfig->Read(wxT("ConfigFilePath"), &pluginConfPathStr);
     wxFileName pluginConfPath (pluginConfPathStr);
@@ -14,29 +15,63 @@ bool dmConfigHandler::LoadConfig(void)
     {
         pluginConfigPath = pluginConfPath;
     }
+    success &= colour.load();
     return success;
 }
 
 bool dmConfigHandler::SaveConfig(void)
 {
     bool success = true;
+    m_pconfig->SetPath(_T("/Settings/LIVI_Depth_model_pi"));
+
     success &= m_pconfig->Write(wxT("ConfigFilePath"), pluginConfigPath.GetFullPath());
+
+    success &= colour.save();
 
     return success;
 
 }
 
+bool DMColorOptionConfig::load(void)
+{
+    bool success = true;    // TODO success of Read functions has not been checked
+    if (confFile)
+    {
+        confFile->SetPath(_T("/Settings/LIVI_Depth_model_pi/Colour"));
+        for (int i = 0; i < DM_NUM_CUSTOM_COL; i++) {
+            std::string colour = confFile->Read(_T("CustomColour") + std::to_string(i));
+            if (sizeof(colour) == 0)
+                colour = "#f0f0f0"; // grey as default, if no color found
+                                    // #ff0000, #ffc4e4, #ffffff, #80c4ff, #0000ff
+
+            unsigned char* valChar = (unsigned char*)colour.c_str();
+            m_customColours[i] = wxColour(valChar);
+        }
+
+        for (int i = 0; i < DM_NUM_CUSTOM_DEP; i++) {
+            std::string str = confFile->Read(_T("CustomDepth" + std::to_string(i)));
+            if (str.length() == 0)
+                str = std::to_string(DM_NUM_CUSTOM_DEP * 5); // grey as default, if no color found
+            m_customDepths[i] = std::stoi(str);
+        }
+    }
+    return success;
+}
+
+bool DMColorOptionConfig::save(void)
+{
     bool success = true;
     if (confFile)
     {
-        confFile->SetPath(_T("/Settings/LIVI_Depth_model_pi/FileImport"));
+        confFile->SetPath(_T("/Settings/LIVI_Depth_model_pi/Colour"));
 
-        success &= confFile->Write(wxT("FileImport_fullpathname"), filePath.GetFullPath());
-
-        success &= confFile->Write(wxT("LatMin"), latMin);
-        success &= confFile->Write(wxT("LatMax"), latMax);
-        success &= confFile->Write(wxT("LonMin"), lonMin);
-        success &= confFile->Write(wxT("LonMax"), lonMax);
+        for (int i = 0; i < DM_NUM_CUSTOM_COL; i++) {
+            success &= confFile->Write(wxT("CustomColour") + std::to_string(i),
+                m_customColours[i].GetAsString(wxC2S_HTML_SYNTAX));
+        }
+        for (int i = 0; i < DM_NUM_CUSTOM_DEP; i++) {
+            success &= confFile->Write(wxT("CustomDepth" + std::to_string(i)), m_customDepths[i]);
+        }
     }
     return success;
 }
--- src/dmConfigHandler.h
+++ src/dmConfigHandler.h
@@ -9,13 +9,25 @@
 #include "wx/wx.h"
 #endif //precompiled headers
 
-//#include <wx/filename.h>
-class wxFileName;
+#include <wx/filename.h>
 class wxFileConfig;
+
+class dm_configAPI
+{
+    virtual bool load() = 0;
+    virtual bool save() = 0;
+
+protected:
+    wxFileConfig* confFile;
+};
 
 #define DM_NUM_CUSTOM_COL 5 // Number of custom colors
 #define DM_NUM_CUSTOM_DEP 4 // Number of custom border depths. Must be equal to DM_NUM_CUSTOM_COL-1.
 struct DMColorOptionConfig : dm_configAPI {
+
+    DMColorOptionConfig(wxFileConfig* confFile)
+    {   this->confFile = confFile;    }
+
     wxColor m_customColours[DM_NUM_CUSTOM_COL]; // 0: col. for always too shallow, 4: col. for always deep enough
     double  m_customDepths[DM_NUM_CUSTOM_DEP]; // 0: level of too shallow, 3: level of always deep enough
     /*
@@ -23,6 +35,8 @@ struct DMColorOptionConfig : dm_configAPI {
     int     m_SlidingDepths[2];     // 0:depth of always too shallow, 1: depth of always deep enough
     int     m_SlidingSteps;         // Number of colour steps from deepest to shallowest
     */
+    virtual bool load() override;
+    virtual bool save() override;
 
     wxColor getColour(int i)             { return m_customColours[i]; }
     void    setColour(int i, wxColor col){ m_customColours[i] = col;  }
@@ -30,12 +44,14 @@ struct DMColorOptionConfig : dm_configAPI {
     double  getDepth(int i)              { return m_customDepths[i];  }
     void    setDepth(int i, double depth){ m_customDepths[i] = depth; }
 };
+
 class dmConfigHandler
 {
 public:
     dmConfigHandler(
-        wxFileConfig* confFile) // change this to reference, to avoid accidentally deleting, 
+        wxFileConfig* confFile) // change this to reference, to avoid accidentally deleting
       : m_pconfig(confFile)
+        colour(confFile)
         showDepthModel(false)
     { 
         /*if debug */assert(confFile);
