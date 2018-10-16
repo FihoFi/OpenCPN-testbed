1c22a0e11dffc4817499a7b940890cf9817441bd
Marjukka Kokkonen <Marjukka.Kokkonen@sitowise.com>
2018-10-16 16:51:17 +0300

    setVisualizationScheme returns value change, and magic starting number to DM_visualization -setVisualizationScheme returns a boolean value, telling if the given enumerator is usable. -The enumerations' starting value is set to a great magic number, to avoid accidentally passing incorrect values.

--- src/dmDataset.h
+++ src/dmDataset.h
@@ -20,7 +20,7 @@ public:
     bool getDatasetExtents(coord &topLeft, coord &botRight) override;
     bool setColourConfigurationFile(const char* filename, bool giveOwnership) override;
     bool setColourConfiguration(const char* fileContents, bool giveOwnership) override;
-    void setVisualizationScheme(DM_visualization visScheme) override;
+    bool setVisualizationScheme(DM_visualization visScheme) override;
     dmRasterImgData * getRasterData(
         coord &topLeftOut, coord &botRightOut) override;
     dmRasterImgData * getRasterData(int imgWidth, int imgHeight,
