01d8722f7df49faf5b18bb4f2b713a9a523830f8
Marjukka Kokkonen <Marjukka.Kokkonen@sitowise.com>
2018-10-26 13:00:12 +0300

    Moving dmExtent definition to its own dmExtent.h file

--- src/dm_API.h
+++ src/dm_API.h
@@ -19,47 +19,6 @@ struct coord {
 };
 
 
-class dmExtent
-{
-    dmExtent();
-    dmExtent(coord topLeft, coord botRight);
-    dmExtent(double top, double left, double bot, double right);
-
-    dmExtent& operator= (const dmExtent& other);
-    bool    operator==(const dmExtent& other);
-
-    /**
-    * @return   true, if area spanned by <i>other> is fully inside
-    *           of this object's spanning area, or if the areas are equal.
-    *           false else.
-    */
-    bool const isWithin(const dmExtent& other);
-
-    /**
-    * @return   true, if the area spanned by <i>other> overlaps with the
-    *           area spanned by this object's, or if the areas are equal.
-    *           false if there is no common point.
-    */
-    bool const overlaps(const dmExtent& other);
-
-    /**
-    * @todo How should we react, if the extents do not overlap?
-    *
-    * @return   The extent spanning the area common to <i>this</i> object,
-    *           and the <i>other</i>.
-    */
-    dmExtent const getSectionWith(const dmExtent& other);
-
-    /**
-    * @todo How should we react, if the extents do not overlap?
-    *
-    * @return   The coordinates of <i>this</i> object, and the <i>other</i>,
-    *           that span the maximal area.
-    */
-    dmExtent const getMaxes(const dmExtent& other);
-
-    coord topLeft;
-    coord botRight;
 };
 
 struct dmRasterImgData
