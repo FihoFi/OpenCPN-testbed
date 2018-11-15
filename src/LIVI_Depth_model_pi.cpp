83b3d086db9352761331c293faaf410447e6c631
Marjukka Kokkonen <Marjukka.Kokkonen@sitowise.com>
2018-11-15 12:52:09 +0200

    Now using pluginDataDir for colour configuration files. Removing duplicate createDMPluginDataPath line.

--- src/LIVI_Depth_model_pi.cpp
+++ src/LIVI_Depth_model_pi.cpp
@@ -115,7 +115,7 @@ int LIVI_Depth_model_pi::Init(void)
     dialog->SetAboutInfo();
 
     m_pconf = new dmConfigHandler(pFileConf, dialog);
-    colourfileHandler = new dmColourfileHandler(m_pconf, *GetpSharedDataLocation());
+
 
     bool success = m_pconf->LoadConfig(); // config variables related to this plugin.
     this->PushConfigToUI();
@@ -131,7 +131,7 @@ int LIVI_Depth_model_pi::Init(void)
         dmDrawer->setTempFileFolder(pluginDataDir);
     } // some error state on failure?
 
-    createDMPluginDataPath();
+    colourfileHandler = new dmColourfileHandler(m_pconf, pluginDataDir.GetPath());
 
     //    This PlugIn needs a toolbar icon, so request its insertion
     if (m_pconf->general.m_bLIVI_Depth_modelShowIcon)
