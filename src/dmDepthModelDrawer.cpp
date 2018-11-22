88c0324c0785555b1894a1a60f6dde00cc93347b
Marjukka Kokkonen <Marjukka.Kokkonen@sitowise.com>
2018-11-22 13:54:56 +0200

    Cosmetics

--- src/dmDepthModelDrawer.cpp
+++ src/dmDepthModelDrawer.cpp
@@ -175,20 +175,10 @@ dmExtent dmDepthModelDrawer::applyViewPortArea(PlugIn_ViewPort &vp)
     return vpLL;
 }
 
-bool dmDepthModelDrawer::isRendering()
-{
-    return renderingDmChart;
-}
+bool dmDepthModelDrawer::isRendering()      {    return renderingDmChart;   }
+void dmDepthModelDrawer::setRenderingOn()   {    renderingDmChart = true;   }
+void dmDepthModelDrawer::setRenderingOff()  {    renderingDmChart = false;  }
 
-void dmDepthModelDrawer::setRenderingOn()
-{
-    renderingDmChart = true;
-}
-
-void dmDepthModelDrawer::setRenderingOff()
-{
-    renderingDmChart = false;
-}
 
 void dmDepthModelDrawer::forceNewImage()
 {
