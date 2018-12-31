74545892f0bc6d0a020f0f5d3f0d537306d100d5
Tommi Larjomaa <tommi.larjomaa@sitowise.com>
2018-12-31 15:48:56 +0200

    Add copyright notices to a few source files

--- src/dmRouteReader.cpp
+++ src/dmRouteReader.cpp
@@ -1,3 +1,29 @@
+/******************************************************************************
+*
+* Project:  OpenCPN
+* Purpose:  LIVI_Depth_model Plugin
+* Author:   Sitowise & LIVI, based on DR_pi example plugin code
+*
+***************************************************************************
+*   Copyright (C) 2018 by Sitowise & LIVI                                 *
+*   $EMAIL$                                                               *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************
+*/
 #include "dmRouteReader.h"
 
 #include "tinyxml.h"
